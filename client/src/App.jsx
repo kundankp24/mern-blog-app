18e2c650de598d378a37fd6470eabb9727639b6d
Kundan Jha <kundan.ext123@gmail.com>
2024-08-12 08:47:28 +0530

    install react js and tailwind css and create the first template

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="bg-slate-500 text-center font-bold text-4xl text-white">
+        Kundan Jha
+      </h1>
+    </>
+  );
+}
+
+export default App;
