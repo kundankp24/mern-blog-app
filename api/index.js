e53b69dba75802ed9b19a69022670b4f036e1db2
kundan <kundan.ext123@gmail.com>
2024-08-14 09:23:34 +0530

    create signup page ui

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ app.listen(3000, ()=>{
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 
-//use of error middileware
+//use of error middleware
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
