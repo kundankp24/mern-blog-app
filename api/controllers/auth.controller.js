942753261fda253e22487be92afb11d693e725b3
kundan <kundan.ext123@gmail.com>
2024-08-17 09:00:57 +0530

    create signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,10 +1,11 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
-  
+
     if (
       !username ||
       !email ||
@@ -26,8 +27,37 @@ export const signup = async (req, res, next) => {
   
     try {
       await newUser.save();
-      res.json('Signup successful');
+      return res.json('Signup successful');
     } catch (error) {
       next(error);
     }
-  };
+};
+
+export const signin= async (req, res, next)=>{
+  const {email, password}= req.body;
+
+  if(!email || !password || email==='' || password===''){
+    next(errorHandler(400, 'All fields are required'))
+  }
+
+  try {
+    const validUser= await User.findOne({email});
+
+    if(!validUser){
+      return next(errorHandler(404, 'User not found'));
+    }
+    const isValidPassword= await bcryptjs.compare(password, validUser.password);
+
+    if(!isValidPassword){
+      return next(errorHandler(400, 'Invalid password'))
+    }
+
+    const token= await jwt.sign({id: validUser._id},process.env.JWT_SECRET);
+
+    const {password: pass, ...rest}= validUser._doc;
+
+    res.status(200).cookie('access-token', token, {httpOnly: true}).json(rest);
+  } catch (error) {
+    next(error);
+  }
+}
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -12,7 +12,7 @@ const FooterComp = () => {
   return (
     <Footer container className="border border-t-8 border-teal-500">
       <div className="w-full max-w-7xl mx-auto">
-        <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+        <div className="grid w-full justify-between sm:flex">
           <div className="mt-5">
             <Link
               to="/"
