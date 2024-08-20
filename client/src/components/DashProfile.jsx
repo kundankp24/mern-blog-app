91613a70361f27109d2610da95f4b18e1498c28d
kundan <kundan.ext123@gmail.com>
2024-08-20 11:35:47 +0530

    complete user image upload functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,50 +1,137 @@
-import { Button, Label, TextInput } from "flowbite-react";
+import { Alert, Button, Label, TextInput } from "flowbite-react";
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { app } from "../firebase";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 const DashProfile = () => {
   const { currentUser } = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
+  const [imageFileUploadError, setImageFileUploadError] = useState(null);
+  const filePickerRef = useRef();
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    setImageFile(file);
+    if (file) {
+      const url = URL.createObjectURL(file);
+      setImageFileUrl(url);
+    }
+  };
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  }, [imageFile]);
+
+  const uploadImage = async () => {
+    setImageFileUploadError(null);
+    const storage = getStorage(app);
+    const filename = new Date().getTime() + imageFile.name;
+    const storageRef = ref(storage, filename);
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setImageFileUploadProgress(progress.toFixed(0));
+      },
+      (error) => {
+        setImageFileUploadError(
+          "Could not upload image (File must be less than 2MB)"
+        );
+        setImageFileUploadProgress(null);
+        setImageFile(null);
+        setImageFileUrl(null);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setImageFileUrl(downloadURL);
+        });
+      }
+    );
+  };
 
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className=" py-7 text-center font-semibold text-3xl">Profile</h1>
       <form className="flex flex-col gap-4">
-        <div className="w-32 h-32 cursor-pointer self-center shadow-md rounded-full overflow-hidden">
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          ref={filePickerRef}
+          hidden
+        />
+        <div
+          className="relative w-32 h-32 cursor-pointer self-center shadow-md rounded-full overflow-hidden "
+          onClick={() => filePickerRef.current.click()}
+        >
+           {imageFileUploadProgress && (
+            <CircularProgressbar
+              value={imageFileUploadProgress || 0}
+              text={`${imageFileUploadProgress}%`}
+              strokeWidth={5}
+              styles={{
+                root: {
+                  width: '100%',
+                  height: '100%',
+                  position: 'absolute',
+                  top: 0,
+                  left: 0,
+                },
+                path: {
+                  stroke: `rgba(62, 152, 199, ${
+                    imageFileUploadProgress / 100
+                  })`,
+                },
+              }}
+            />
+          )}
           <img
-            src={currentUser.profilePicture}
+            src={imageFileUrl || currentUser.profilePicture}
             alt="user"
-            className="rounded-full w-full h-full object-cover border-8 border-[lightgray]"
+            className={`rounded-full w-full h-full object-cover border-8 border-[lightgray]`}
           />
         </div>
+        {imageFileUploadError && (
+          <Alert color="failure">{imageFileUploadError}</Alert>
+        )}
         <TextInput
           type="text"
           id="username"
           placeholder="username"
           defaultValue={currentUser.username}
         />
-         <TextInput
+        <TextInput
           type="email"
           id="email"
           placeholder="email"
           defaultValue={currentUser.email}
         />
-         <TextInput
-          type="password"
-          id="password"
-          placeholder="password"
-        />
-        <Button type="submit" gradientDuoTone='purpleToBlue' outline>
-            Update
+        <TextInput type="password" id="password" placeholder="password" />
+        <Button type="submit" gradientDuoTone="purpleToBlue" outline>
+          Update
         </Button>
       </form>
       <div className="flex justify-between mt-5">
-  <span className="cursor-pointer text-red-500 hover:text-red-700 transition-colors duration-300 font-semibold">
-    Delete Account
-  </span>
-  <span className="cursor-pointer text-blue-500 hover:text-blue-700 transition-colors duration-300 font-semibold">
-    Sign Out
-  </span>
-</div>
-
+        <span className="cursor-pointer text-red-500 hover:text-red-700 transition-colors duration-300 font-semibold">
+          Delete Account
+        </span>
+        <span className="cursor-pointer text-blue-500 hover:text-blue-700 transition-colors duration-300 font-semibold">
+          Sign Out
+        </span>
+      </div>
     </div>
   );
 };
--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,10 +1,9 @@
 import { Sidebar } from "flowbite-react";
-import { useEffect, useState } from 'react'
-import {Link, useLocation} from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import { HiArrowSmRight, HiUser } from "react-icons/hi";
 
 const DashSidebar = () => {
-
   const location = useLocation();
   const [tab, setTab] = useState("");
 
@@ -20,9 +19,15 @@ const DashSidebar = () => {
     <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
         <Sidebar.ItemGroup>
-          <Link to='/dashboard?tab=profile'>
-            <Sidebar.Item active={tab==='profile'} icon={HiUser} label={"user"} labelColor="dark">
-                Profile
+          <Link to="/dashboard?tab=profile">
+            <Sidebar.Item
+              active={tab === "profile"}
+              icon={HiUser}
+              label={"user"}
+              labelColor="dark"
+              as="button"
+            >
+              Profile
             </Sidebar.Item>
           </Link>
           <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer">
