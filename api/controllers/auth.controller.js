9e27554415f1112105374cce059952aa1c4eb71b
kundan <kundan.ext123@gmail.com>
2024-08-24 10:26:14 +0530

    add admin functionality to api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -52,7 +52,7 @@ export const signin= async (req, res, next)=>{
       return next(errorHandler(400, 'Invalid password'))
     }
 
-    const token= await jwt.sign({id: validUser._id},process.env.JWT_SECRET);
+    const token= await jwt.sign({id: validUser._id, isAdmin: validUser.isAdmin},process.env.JWT_SECRET);
 
     const {password: pass, ...rest}= validUser._doc;
 
@@ -70,7 +70,7 @@ export const google= async (req, res, next)=>{
     const user= await User.findOne({email});
 
     if(user){
-      const token= await jwt.sign({id: user._id}, process.env.JWT_SECRET);
+      const token= await jwt.sign({id: user._id, isAdmin: user.isAdmin}, process.env.JWT_SECRET);
       const {password, ...rest}= user._doc;
 
       return res.status(200).cookie('access_token', token, {
@@ -89,7 +89,7 @@ export const google= async (req, res, next)=>{
         profilePicture: googlePhotoUrl
       });
       await newUser.save();
-      const token= await jwt.sign({id: newUser._id}, process.env.JWT_SECRET);
+      const token= await jwt.sign({id: newUser._id, isAdmin: newUser.isAdmin}, process.env.JWT_SECRET);
   
       const {password, ...rest}= newUser._doc;
 
